627f8a7da45d0f5e219a99bb9ce00912ff42b285
munxtwo <munxtwo@gmail.com>
2013-07-19 00:09:23 -0700

    Add db script to populate values in genre_id column

---
+++ src/main/resources/db_scripts/03_set_values_tmovies_genreid_column.sql
@@ -0,0 +1,12 @@
+-- Update genre_id column in TMOVIES
+UPDATE TMOVIES set genre_id=5 where name="Man of Steel";
+UPDATE TMOVIES set genre_id=5 where name="Fast & Furious 6";
+UPDATE TMOVIES set genre_id=1 where name="The Internship";
+UPDATE TMOVIES set genre_id=6 where name="The Shawshank Redemption";
+UPDATE TMOVIES set genre_id=5 where name="The Dark Knight";
+UPDATE TMOVIES set genre_id=6 where name="The Lord of the Rings: The Return of the King";
+UPDATE TMOVIES set genre_id=5 where name="Inception";
+UPDATE TMOVIES set genre_id=5 where name="The Matrix";
+UPDATE TMOVIES set genre_id=6 where name="Game of Thrones";
+UPDATE TMOVIES set genre_id=1 where name="Friends";
+UPDATE TMOVIES set genre_id=1 where name="How I Met Your Mother";
